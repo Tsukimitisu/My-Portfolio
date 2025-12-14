c903772b6342dddc31e9fd5f66267a1ee6b6f43c
James Andrei Revilla <revillajamesandrei4@gmail.com>
2025-12-15 04:48:22 +0800

    change image

--- server.js
+++ server.js
@@ -3,13 +3,12 @@ const fs = require('fs');
 const path = require('path');
 
 const server = http.createServer((req, res) => {
-    // Build file path
+   
     let filePath = path.join(__dirname, req.url === '/' ? 'index.html' : req.url);
     
-    // Get extension
+
     let extname = path.extname(filePath);
     
-    // Content type defaults
     let contentType = 'text/html';
     switch (extname) {
         case '.js': contentType = 'text/javascript'; break;
@@ -19,22 +18,22 @@ const server = http.createServer((req, res) => {
         case '.jpg': contentType = 'image/jpg'; break;
     }
 
-    // Read file
+    
     fs.readFile(filePath, (err, content) => {
         if (err) {
             if (err.code == 'ENOENT') {
-                // Page not found
+              
                 fs.readFile(path.join(__dirname, '404.html'), (err, content) => {
                     res.writeHead(200, { 'Content-Type': 'text/html' });
                     res.end("404 Not Found", 'utf8');
                 });
             } else {
-                // Server error
+               
                 res.writeHead(500);
                 res.end(`Server Error: ${err.code}`);
             }
         } else {
-            // Success
+           
             res.writeHead(200, { 'Content-Type': contentType });
             res.end(content, 'utf8');
         }
